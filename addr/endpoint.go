56f844d346b10b8135f59855a79653f037201860
unknown <kokweikhong@gmail.com>
2021-09-03 23:02:50 +0800

    add order endpoint

--- addr/endpoint.go
+++ addr/endpoint.go
@@ -1,13 +1,20 @@
 package addr
 
 const (
-	livehost          = "https://api-fxtrade.oanda.com"
+	livehost = "https://api-fxtrade.oanda.com"
+
+	// Account endpoints
 	Accounts          = livehost + "/v3/accounts"
 	AccountsById      = livehost + "/v3/accounts/%v"
 	AccountSummary    = livehost + "/v3/accounts/%v/summary"
 	AccountInstrument = livehost + "/v3/accounts/%v/instruments"
 
+	// Instrument endpoints
 	InstrumentCandles      = livehost + "/v3/instruments/%v/candles"
 	InstrumentOrderBook    = livehost + "/3/instruments/%v/orderBook"
 	InstrumentPositionBook = livehost + "/v3/instruments/%v/positionBook"
+
+	// Order endpoints
+	Orders        = livehost + "/v3/accounts/%v/orders"
+	PendingOrders = livehost + "/v3/accounts/%v/pendingOrders"
 )
