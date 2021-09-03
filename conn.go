4843bf2736e2011a86e2b42f5f6e0c06624b7e1c
unknown <kokweikhong@gmail.com>
2021-09-03 23:05:12 +0800

    first commit

--- conn.go
+++ conn.go
@@ -12,18 +12,19 @@ type connection struct {
 	endpoint string
 	method   string
 	token    string
+	data     []byte
 }
 
 func (co *connection) connect() []byte {
 	client := &http.Client{Timeout: 2 * time.Second}
-	req, err := http.NewRequest(co.method, co.endpoint, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var buffer bytes.Buffer
 	buffer.WriteString("Bearer ")
 	buffer.WriteString(co.token)
 	auth := buffer.String()
+	req, err := http.NewRequest(co.method, co.endpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	req.Header.Set("User-Agent", "v20-golang/0.1")
 	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
