799834e0b51b26526a9403c596b40799890c0946
unknown <kokweikhong@gmail.com>
2021-09-10 12:12:38 +0800

    add practice url

--- addr/endpoint.go
+++ addr/endpoint.go
@@ -1,27 +1,30 @@
 package addr
 
 const (
-	streamhost = "https://stream-fxtrade.oanda.com"
-	livehost   = "https://api-fxtrade.oanda.com"
+	streamApi    = "https://stream-%v.oanda.com"
+	restApi      = "https://api-%v.oanda.com"
+	LiveHost     = "fxtrade"
+	PracticeHost = "fxpractice"
 
+	// https://api-fxpractice.oanda.com
 	// Account endpoints
-	Accounts          = livehost + "/v3/accounts"
-	AccountsById      = livehost + "/v3/accounts/%v"
-	AccountSummary    = livehost + "/v3/accounts/%v/summary"
-	AccountInstrument = livehost + "/v3/accounts/%v/instruments"
+	Accounts          = restApi + "/v3/accounts"
+	AccountsById      = restApi + "/v3/accounts/%v"
+	AccountSummary    = restApi + "/v3/accounts/%v/summary"
+	AccountInstrument = restApi + "/v3/accounts/%v/instruments"
 
 	// Instrument endpoints
-	InstrumentCandles      = livehost + "/v3/instruments/%v/candles"
-	InstrumentOrderBook    = livehost + "/3/instruments/%v/orderBook"
-	InstrumentPositionBook = livehost + "/v3/instruments/%v/positionBook"
+	InstrumentCandles      = restApi + "/v3/instruments/%v/candles"
+	InstrumentOrderBook    = restApi + "/3/instruments/%v/orderBook"
+	InstrumentPositionBook = restApi + "/v3/instruments/%v/positionBook"
 
 	// Order endpoints
-	Orders        = livehost + "/v3/accounts/%v/orders"
-	PendingOrders = livehost + "/v3/accounts/%v/pendingOrders"
+	Orders        = restApi + "/v3/accounts/%v/orders"
+	PendingOrders = restApi + "/v3/accounts/%v/pendingOrders"
 
 	// Pricing endpoints
-	PrCandleLatest       = livehost + "/v3/accounts/%v/candles/latest"
-	PrPricingInfo        = livehost + "/v3/accounts/%v/pricing"
-	PrPricingStream      = streamhost + "/v3/accounts/%v/pricing/stream"
-	PrInstrumentCandlees = livehost + "/v3/accounts/%v/instruments/instrument/candles"
+	PrCandleLatest       = restApi + "/v3/accounts/%v/candles/latest"
+	PrPricingInfo        = restApi + "/v3/accounts/%v/pricing"
+	PrPricingStream      = streamApi + "/v3/accounts/%v/pricing/stream"
+	PrInstrumentCandlees = restApi + "/v3/accounts/%v/instruments/instrument/candles"
 )
