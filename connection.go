12956384273c40397381ffecaa62a1b843bb1870
unknown <kokweikhong@gmail.com>
2021-10-02 12:57:55 +0800

    modify errors

--- connection.go
+++ connection.go
@@ -21,23 +21,21 @@ func (co *connection) connect() ([]byte, error) {
 	buffer.WriteString("Bearer ")
 	buffer.WriteString(co.token)
 	auth := buffer.String()
-	fmt.Println(co)
 	req, err := http.NewRequest(co.method, co.endpoint, bytes.NewBuffer(co.data))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to request api from %v, %v", co.endpoint, err)
 	}
 	// req.Header.Set("User-Agent", "v20-golang/0.1")
 	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to request api after set token, %v", err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get response from %v, %v", co.endpoint, err)
 	}
-	fmt.Println(string(body))
 	return body, nil
 }
